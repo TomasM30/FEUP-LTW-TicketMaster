342639eff98a0f79465c464344743b3a86727054
Tomás Martins <tomas.eiras03@gmail.com>
2023-05-16 00:44:27 +0100

    removing 'var' variable from js.dont use var

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,13 +1,13 @@
 function opacityPointerA(){
-    var elements = document.querySelectorAll('footer, header, .profileContainer');
-    for (var i = 0; i < elements.length; i++) {
+    const elements = document.querySelectorAll('footer, header, .profileContainer');
+    for (let i = 0; i < elements.length; i++) {
         elements[i].style.pointerEvents = 'none';
         elements[i].style.opacity = '0.5';
     }
 }
 function opacityPointerD(){
-    var elements = document.querySelectorAll('footer, header, .profileContainer');
-    for (var i = 0; i < elements.length; i++) {
+    const elements = document.querySelectorAll('footer, header, .profileContainer');
+    for (let i = 0; i < elements.length; i++) {
         elements[i].style.pointerEvents = '';
         elements[i].style.opacity = '';
     }
@@ -48,7 +48,7 @@ function scrollHContainer(scrollAmount,direction,container) {
 }
 
 function openStatusMenu(){
-    var form = document.getElementById('statusChangeForm');
+    const form = document.getElementById('statusChangeForm');
     if (form.style.display === 'block') {
         form.style.display = 'none';
     } else {
@@ -56,7 +56,7 @@ function openStatusMenu(){
     }
 }
 function openAgentsMenu(){
-    var form = document.getElementById('agentChangeForm');
+    const form = document.getElementById('agentChangeForm');
     if (form.style.display === 'block') {
         form.style.display = 'none';
     } else {
@@ -64,7 +64,7 @@ function openAgentsMenu(){
     }
 }
 function openDepartmentMenu(){
-    var form = document.getElementById('departmentChangeForm');
+    const form = document.getElementById('departmentChangeForm');
     if (form.style.display === 'block') {
         form.style.display = 'none';
     } else {
@@ -72,7 +72,7 @@ function openDepartmentMenu(){
     }
 }
 function openPriorityMenu(){
-    var form = document.getElementById('priorityChangeForm');
+    const form = document.getElementById('priorityChangeForm');
     if (form.style.display === 'block') {
         form.style.display = 'none';
     } else {
